810413a0d0beb44d0ee31a4f714a4cf88f9831a8
Jacob Isah <isahjacob0@gmail.com>
2024-07-03 23:38:51 +0100

    remove commented out code

--- resumeBuilder_app/models.py
+++ resumeBuilder_app/models.py
@@ -45,7 +45,6 @@ class Resume(models.Model):
     template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # Contact Info
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     job_title = models.CharField(max_length=100)
