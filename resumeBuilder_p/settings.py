ef62b0693444f6b691f95eb3f0674fd223aded47
Jacob Isah <isahjacob0@gmail.com>
2024-08-09 10:38:42 +0100

    add render url to  cors allowed host

--- resumeBuilder_p/settings.py
+++ resumeBuilder_p/settings.py
@@ -38,7 +38,8 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # ALLOWED_HOSTS = ["*"]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1',
