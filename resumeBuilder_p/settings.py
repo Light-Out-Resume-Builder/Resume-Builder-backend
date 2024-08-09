9c83378db5dffb4fe90312524ef010447966bbd4
Jacob Isah <isahjacob0@gmail.com>
2024-08-09 10:22:29 +0100

    add render url to  cors allowed host

--- resumeBuilder_p/settings.py
+++ resumeBuilder_p/settings.py
@@ -43,7 +43,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1',
     'http://localhost:3000',
-    'https://resumesbuilder.netlify.app/'
+    'https://resumesbuilder.netlify.app/',
+    'https://resume-builder-backend-wvco.onrender.com'
 ]
 
 
