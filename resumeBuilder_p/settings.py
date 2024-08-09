9322f61d4e846eb0204880c0d4d7733eb3c5993f
Jacob Isah <isahjacob0@gmail.com>
2024-08-09 10:06:20 +0100

    add cors allowed host

--- resumeBuilder_p/settings.py
+++ resumeBuilder_p/settings.py
@@ -34,11 +34,17 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 Debug = os.environ.get("DEBUG", False).lower() == "true"
 
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1',
+    'http://localhost:3000',
+    'https://resumesbuilder.netlify.app/'
+]
 
 
 CORS_ALLOWED_ORIGINS = [
