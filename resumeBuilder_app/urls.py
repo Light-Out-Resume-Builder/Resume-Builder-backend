ef7291b9e14d4ce6b3ae48d54ae9e54ae596910e
Jacob Isah <isahjacob0@gmail.com>
2024-08-09 12:17:33 +0100

    remove openai api documentation
    modify the resume endpoints to use id for get, update delete

--- resumeBuilder_app/urls.py
+++ resumeBuilder_app/urls.py
@@ -1,44 +1,28 @@
 from . import views
 from django.urls import path
-from .views import TemplateList, TemplateDetail
+from .views import TemplateList, TemplateDetail, ResumeViewSet
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-app_name = "resumeBuilder_app"
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@local.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 
 urlpatterns = [
-    path('templates/', TemplateList.as_view()),
-    path('templates/<int:pk>/', TemplateDetail.as_view()),
-    # path('templates/<int:pk>/download/', views.TemplateDownloadView.as_view()),
-    # path('templates/<int:pk>/preview/', views.TemplatePreviewView.as_view()),
+    # path('templates/', TemplateList.as_view()),
+    # path('templates/<int:pk>/', TemplateDetail.as_view()),
+    # # path('templates/<int:pk>/download/', views.TemplateDownloadView.as_view()),
+    # # path('templates/<int:pk>/preview/', views.TemplatePreviewView.as_view()),
 
-    path('resume/', views.ResumeViewSet.as_view({
-        'get': 'list',
-        'post': 'create',
-        'delete': 'destroy',
-        'put': 'update',
-    })),
+    path('resume/', ResumeViewSet.as_view({
+        'get': 'list',      # To Handles GET requests to list all resumes
+        'post': 'create'    # To Handles POST requests to create a new resume
+    }), name="resume-list"),
 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('resume/<int:pk>/', ResumeViewSet.as_view({
+        'get': 'retrieve',  # To Handles GET requests to retrieve a single resume by ID
+        'put': 'update',    # To Handles PUT requests to update a resume by ID
+        'delete': 'destroy' # To Handles DELETE requests to delete a resume by ID
+    }), name="resume-detail"),
     
     path('register/',views.RegisterView.as_view(),name="register"),
     path('login/',views.LoginView.as_view(),name="login"),
