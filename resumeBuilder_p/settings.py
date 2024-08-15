d9c890abc137da20e07db5b8284183ecef47daa4
Jacob Isah <isahjacob0@gmail.com>
2024-08-15 21:23:43 +0100

    add vite local host

--- resumeBuilder_p/settings.py
+++ resumeBuilder_p/settings.py
@@ -44,6 +44,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1',
     'http://localhost:3000',
+    'http://localhost:5173',
     'https://resumesbuilder.netlify.app',
     'https://resume-builder-backend-wvco.onrender.com'
 ]
