b04d1eedc8270ea827c40326abc88adff6e70204
Jacob Isah <isahjacob0@gmail.com>
2024-08-22 16:42:28 +0100

    added local host to allowed host

--- resumeBuilder_p/settings.py
+++ resumeBuilder_p/settings.py
@@ -43,6 +43,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1',
+    'localhost',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
