79140e98166e66e9bca959a3407b0a6afa55613d
Jacob Isah <isahjacob0@gmail.com>
2024-08-26 10:13:33 +0100

    modify the serializers for sigup, login and logout

--- resumeBuilder_app/serializers.py
+++ resumeBuilder_app/serializers.py
@@ -1,76 +1,32 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib import auth
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from django.contrib.auth import authenticate
-from rest_framework.exceptions import AuthenticationFailed
 from .models import Template, Resume
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, max_length=128, min_length=6)
-
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['fullname', 'email', 'password']
-
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError('Email is already in use')
-        return value
+        fields = ('id', 'email', 'fullname', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data['email'],
-            password=validated_data['password'],
-            fullname=validated_data['fullname']
+            fullname=validated_data['fullname'],
+            password=validated_data['password']
         )
         return user
 
 
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(max_length=255, min_length=3)
-    password = serializers.CharField(max_length=128, min_length=6, write_only=True)
-    fullname = serializers.CharField(max_length=255, read_only=True)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
 
-    def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
-
-        if email and password:
-            user = authenticate(request=self.context.get('request'), email=email, password=password)
-            if not user:
-                raise AuthenticationFailed('Invalid credentials, try again')
-        else:
-            raise serializers.ValidationError('Must include "email" and "password"')
-
-        attrs['user'] = user
-        return attrs
-
-    def get_tokens_for_user(self, user):
-        refresh = RefreshToken.for_user(user)
-        return {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token)
-        }
-
-
-class LogoutSerializer(serializers.Serializer):
-    refresh = serializers.CharField()
-
-    default_error_messages = {
-        'bad_token': 'Token is expired or invalid'
-    }
-
-    def validate(self, attrs):
-        self.token = attrs.get('refresh')
-        return attrs
-
-    def save(self):
-        try:
-            RefreshToken(self.token).blacklist()
-        except TokenError:
-            self.fail('bad_token')
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
 
 
 
