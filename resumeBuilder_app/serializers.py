6157376aa8c695a90d67cb143100cd1e4cdf0e8d
Jacob Isah <isahjacob0@gmail.com>
2024-08-27 17:24:04 +0100

    updated user serializers

--- resumeBuilder_app/serializers.py
+++ resumeBuilder_app/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
-from .models import User
+# from .models import User
 from django.contrib.auth import authenticate
 from .models import Template, Resume
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'fullname', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'email', 'fullname']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'fullname']
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -17,20 +29,26 @@ class UserSerializer(serializers.ModelSerializer):
         )
         return user
 
-
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     def validate(self, data):
         email = data.get('email')
         password = data.get('password')
 
-        user = authenticate(email=email, password=password)
-        if user is None:
-            raise serializers.ValidationError('Invalid credentials')
+        if not email or not password:
+            raise serializers.ValidationError('Email and password are required.')
 
-        return {'user': user}
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError('User with this email does not exist.')
+
+        if not user.check_password(password):
+            raise serializers.ValidationError('Incorrect password.')
+
+        return user
 
 
 
